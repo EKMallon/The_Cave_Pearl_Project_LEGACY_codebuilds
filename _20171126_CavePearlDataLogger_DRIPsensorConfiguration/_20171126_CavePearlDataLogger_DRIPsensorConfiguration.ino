092cf41a11228645a03737d119ebca60a7ec8df6
The Cave Pearl Project <EKMallon@users.noreply.github.com>
2018-03-04 09:18:58 -0600

    update resistor factor

--- _20171126_CavePearlDataLogger_DRIPsensorConfiguration/_20171126_CavePearlDataLogger_DRIPsensorConfiguration.ino
+++ _20171126_CavePearlDataLogger_DRIPsensorConfiguration/_20171126_CavePearlDataLogger_DRIPsensorConfiguration.ino
@@ -109,7 +109,7 @@ int freeMem = 9999;       // dynamic memory at run time is always less than the 
 uint16_t batteryVoltage = 9999;     //the main battery voltage (via 1.1 internal band gap OR analog read)
 #define batteryPin A0               //where you have connected the voltage divider tracking the main battery voltage
 int postSDbatt = 9999;         //the post SD card writing main battery voltage, 9999 until first card write cycle
-const float resistorFactor = 511.5; // = 1023.0 * (R2/(R1 + R2)); // 511.5 if both resistors equal // 255.75 Assumes (high side=10Meg ohm)/(low side=3.3Meg ohm)divider
+const float resistorFactor = 511.5; // = 1023.0 * (R2/(R1 + R2)); // 511.5 if both resistors equal // 253.82 Assumes (high side=10Meg ohm)/(low side=3.3Meg ohm)divider
 const float referenceVolts = 3.30;  // You need to check this with a DVM for each board to get accurate battery readings.
 uint16_t CoinCellV = 9999;          // On SOME loggers I have voltage divider on the RTC coin cell connected to line A1 - this reading occurs only once per day
 uint16_t VccBGap = 0;               //tracks of the main regulators output voltage using the internal 1.1v bandgap trick
